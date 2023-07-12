3d7b13ac4d3a99679d0f885b945f10d3538ff95f
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-12 17:18:37 +0530

    nav bar created

--- car land frontend/src/header/Header.tsx
+++ car land frontend/src/header/Header.tsx
@@ -1,26 +1,188 @@
-import './header.css'
-function Header():JSX.Element {
-  return (
-   <>
-  
-  <div className="box">
-        <div className="header-wrapper">
-          <header className="header">
-            <div className="text-wrapper">menu</div>
-            <img className="link" alt="Link" src="" />
-            <div className="nav-list">
-              <div className="item-link">
-                <img className="img" alt="Img" src="" />
-                <div className="div">Become a Host</div>
-              </div>
-              <div className="item-link-login">Login/Signup</div>
-            </div>
-          </header>
-        </div>
-      </div>
+import React, { useState } from 'react';
 
-   </>
-  )
+function Header() {
+    const [isToggled, setIsToggled] = useState(false);
+    const [isToggled2, setIsToggled2] = useState(true);
+    const handleClick = () => {
+        setIsToggled(!isToggled);
+    };
+    const handleClick2 = () => {
+        console.log("hai");
+
+        setIsToggled2(!isToggled2);
+    };
+    return (
+        <>
+            <nav className="bg-gray-200 border-gray-200 dark:bg-gray-900">
+                <div className="max-w-screen-2xxl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="http://localhost:3000" className="flex items-center">
+                        <img
+                            src="https://media.istockphoto.com/id/1201498022/vector/machine-car-line-icon-elements-of-wedding-illustration-icons-signs-symbols-can-be-used-for.jpg?s=612x612&w=is&k=20&c=Wmx6mwP9i_p4SPE22k6sakbfkdb7YQymY2Y-0qbOoK4="
+                            className="h-10 w-10"
+                            alt="Carlan Logo"
+                        />
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                            Car Land
+                        </span>
+                    </a>
+
+
+
+
+                    <div className="flex items-center md:order-2 space-x-4">
+                        <span>Risvan</span>
+                        <button
+                            type="button"
+                            onClick={handleClick}
+                            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                            id="user-dropdown"
+                            aria-expanded="false"
+                            data-dropdown-toggle="user-dropdown"
+                            data-dropdown-placement="bottom"
+                        >
+                            <span className="sr-only">Open user menu</span>
+                            <img
+                                className="w-8 h-8 rounded-full"
+                                src="https://img.freepik.com/free-photo/luxurious-car-parked-highway-with-illuminated-headlight-sunset_181624-60607.jpg"
+                                alt="user photo"
+                            />
+                        </button>
+
+                        {/* <!-- Dropdown menu --> */}
+
+                        <button
+                            data-collapse-toggle="user-menu-button"
+                            type="button"
+                            onClick={handleClick2}
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            aria-expanded="false"
+                            id='user-menu-button'
+                            data-dropdown-toggle="user-menu-button"
+                            data-dropdown-placement="bottom"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <svg
+                                className="w-5 h-5"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 17 14"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M1 1h15M1 7h15M1 13h15"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div
+                        className=' items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
+                        id="user-menu-button"
+                    >
+                        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pl-3 pr-4 text-white bg-grey-200 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                                    aria-current="page"
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                >
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                >
+                                    Services
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                >
+                                    Pricing
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                >
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+
+            </nav>
+
+
+            <div
+                className={`z-1 ${isToggled ? 'block' : 'hidden'
+                    } my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 absolute h-50 w-25 top-10 end-0 `}
+                id="user-dropdown"
+            >
+                <div className="px-4 py-3">
+                    <span className="block text-sm text-gray-900 dark:text-white">
+                        Bonnie Green
+                    </span>
+                    <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                        name@flowbite.com
+                    </span>
+                </div>
+                <ul className="py-2" aria-labelledby="user-menu-button">
+                    <li>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                            Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                            Settings
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                            Earnings
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                            Sign out
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
 }
 
-export default Header
+export default Header;
--- car land frontend/tailwind.config.js
+++ car land frontend/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
+
   theme: {
     extend: {},
   },
-  plugins: [],
-
-}
-
+  plugins: [require("flowbite/plugin")],
+};
