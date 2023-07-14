ceda060eaa45efcfeb943dd95da96651cbaf5ceb
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-14 14:14:15 +0530

    mongo atlas connected with my project

---
+++ car land backend/src/config/mongoDB.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+
+export const  DBconnect=():void=>{
+    mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => console.log("db connected"))
+    .catch((error) => console.log(error));
+
+}
+   
