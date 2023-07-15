261dca871c02f47d4a9daa200932441ed8dbfc10
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-15 12:31:28 +0530

    created buttton for changing component using state

--- car land frontend/src/components/loginpage/LoginMainPage.tsx
+++ car land frontend/src/components/loginpage/LoginMainPage.tsx
@@ -65,12 +65,15 @@ export const LoginMainPage = (): ReactElement => {
               <div className="px-3 text-xs text-gray-500 sm:text-sm">or</div>
               <hr className="flex-1 border-gray-300" />
             </div>
-            <a
-              href="/"
+            <button
+            onClick={()=>{
+              console.log("hai");
+              
+            }}
               className="inline-flex items-center justify-center w-full h-12 px-6 font-semibold transition duration-200 bg-white border border-gray-300 rounded md:w-auto hover:bg-gray-100 focus:shadow-outline focus:outline-none"
             >
               Sign Up with Email
-            </a>
+            </button>
           </div>
 
         </div>
