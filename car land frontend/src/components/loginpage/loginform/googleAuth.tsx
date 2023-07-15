233e39701016720afd6bf9480394f26d439a07b2
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-15 15:22:46 +0530

    google Auth component  created

---
+++ car land frontend/src/components/loginpage/loginform/googleAuth.tsx
@@ -0,0 +1,60 @@
+
+
+export const googleAuth = () => {
+  return (
+   <>
+   <div className="px-5 pt-6 pb-5 text-center border border-gray-300 rounded lg:w-2/5">
+            <div className="mb-5 font-semibold">Create an account</div>
+            <div className="flex justify-center w-full mb-3">
+              <button
+
+                className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-black hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+              >
+                <div className="flex items-center">
+                  <div className="mr-3 font-semibold text-white">
+                    Login with Google
+                  </div>
+                  <svg viewBox="0 0 48 48" width="24px" height="24px">
+                    <path fill="#FFC107" d="M41.44,20.184c0-1.424-0.127-2.807-0.369-4.137H24v7.851h10.959c-0.455,2.563-2.024,4.725-4.464,5.877v4.878h7.218 C39.681,33.693,41.44,27.988,41.44,20.184z" />
+                    <path fill="#FF3D00" d="M24,44.871c6.479,0,11.979-2.117,16.001-5.759l-7.718-6.033c-1.773,1.192-4.038,1.902-6.538,1.902 c-5.033,0-9.301-3.385-10.8-7.939H3.876v4.978C8.041,39.297,15.605,44.871,24,44.871z" />
+                    <path fill="#4CAF50" d="M13.203,26.322c-0.325-0.966-0.498-2.002-0.498-3.078s0.173-2.112,0.498-3.078V14.18H3.876 C2.549,16.499,2,19.11,2,21.826s0.549,5.327,1.876,7.646L13.203,26.322z" />
+                    <path fill="#1976D2" d="M24,8.129c2.867,0,5.46,1.019,7.507,2.688l5.605-5.605C35.977,2.565,30.479,0,24,0 C15.605,0,8.041,5.574,5.482,13.212l7.218,5.666C14.699,11.514,19.967,8.129,24,8.129z" />
+                    <path fill="#FF6D00" d="M5.482,13.212L5.482,13.212l-2.876,3.697C2.223,17.843,2,19.305,2,20.826s0.223,2.983,0.606,4.118 l7.218-5.666C8.041,18.426,15.605,24,24,24v-4.129C19.967,19.871,14.699,16.486,5.482,13.212z" />
+                  </svg>
+
+                </div>
+              </button>
+            </div>
+            <p className="max-w-md px-5 mb-3 text-xs text-gray-600 sm:text-sm md:mb-5">
+              Sed ut unde omnis iste natus accusantium doloremque laudantium omnis
+              iste.
+            </p>
+            <div className="flex items-center w-full mb-5">
+              <hr className="flex-1 border-gray-300" />
+              <div className="px-3 text-xs text-gray-500 sm:text-sm">or</div>
+              <hr className="flex-1 border-gray-300" />
+            </div>
+            <button
+              onClick={() => {
+                console.log("hai");
+
+              }}
+              className="inline-flex items-center justify-center w-full h-12 px-6 font-semibold transition duration-200 bg-white border border-gray-300 rounded md:w-auto hover:bg-gray-100 focus:shadow-outline focus:outline-none"
+            >
+              Sign In with Email
+            </button>
+            <button
+              onClick={() => {
+                console.log("hai");
+
+              }}
+              className="inline-flex items-center justify-center w-full h-12 px-6 font-semibold transition duration-200 bg-white border border-gray-300 rounded md:w-auto hover:bg-gray-100 focus:shadow-outline focus:outline-none"
+            >
+              Sign Up with Email
+            </button>
+          </div>
+   </>
+  )
+}
+
+ 
--- car land frontend/src/components/userHeader/MainHeader/MainHeader.tsx
+++ car land frontend/src/components/userHeader/MainHeader/MainHeader.tsx
@@ -117,9 +117,9 @@ export const MainHeader= ():ReactElement => {
               </svg>
             </button>
             {isMenuOpen && (
-              <div className="absolute top-0 left-0 w-full">
-                <div className="p-5 bg-white border rounded shadow-sm">
-                  <div className="flex items-center justify-between mb-4">
+              <div className="absolute top-0 left-0 w-full ">
+                <div className="p-5 bg-black text-white border rounded shadow-sm">
+                  <div className="flex items-center text-white justify-between mb-4">
                     <div>
                       <a
                         href="/"
@@ -142,7 +142,7 @@ export const MainHeader= ():ReactElement => {
                           <rect x="14" y="1" width="7" height="6" />
                           <rect x="14" y="11" width="7" height="12" />
                         </svg>
-                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                        <span className="ml-2 text-xl text-white font-bold tracking-wide text-gray-800 uppercase">
                           Company
                         </span>
                       </a>
@@ -151,10 +151,10 @@ export const MainHeader= ():ReactElement => {
                       <button
                         aria-label="Close Menu"
                         title="Close Menu"
-                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        className="p-2 -mt-2 -mr-2 transition duration-200  rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                         onClick={() => setIsMenuOpen(false)}
                       >
-                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                        <svg className="w-5 text-white" viewBox="0 0 24 24">
                           <path
                             fill="currentColor"
                             d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -164,13 +164,13 @@ export const MainHeader= ():ReactElement => {
                     </div>
                   </div>
                   <nav>
-                    <ul className="space-y-4">
+                    <ul className="space-y-4 text-white">
                       <li>
                         <a
                           href="/"
                           aria-label="Our product"
                           title="Our product"
-                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          className="font-medium tracking-wide  transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
                           Product
                         </a>
@@ -180,7 +180,7 @@ export const MainHeader= ():ReactElement => {
                           href="/"
                           aria-label="Our product"
                           title="Our product"
-                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          className="font-medium tracking-wide  transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
                           Features
                         </a>
@@ -190,7 +190,7 @@ export const MainHeader= ():ReactElement => {
                           href="/"
                           aria-label="Product pricing"
                           title="Product pricing"
-                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          className="font-medium tracking-wide  transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
                           Pricing
                         </a>
@@ -200,20 +200,19 @@ export const MainHeader= ():ReactElement => {
                           href="/"
                           aria-label="About us"
                           title="About us"
-                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          className="font-medium tracking-wide transition-colors duration-200 hover:text-deep-purple-accent-400"
                         >
                           About us
                         </a>
                       </li>
                       <li>
-                        <a
-                          href="/"
+                        <button
                           className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                           aria-label="Sign up"
                           title="Sign up"
                         >
                           Sign up
-                        </a>
+                        </button>
                       </li>
                     </ul>
                   </nav>
