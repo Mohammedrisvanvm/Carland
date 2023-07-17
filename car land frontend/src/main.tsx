0b50e8dd92de7ab4f9fffce56d28fe7a18b2e2d2
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-17 11:21:59 +0530

    reduc toolkit store configured

--- car land frontend/src/main.tsx
+++ car land frontend/src/main.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { Store } from './redux/store/store.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider store={Store}>
+
+   
     <App />
+    </Provider >
    
   </React.StrictMode>,
 )
---
+++ car land frontend/src/redux/store/store.tsx
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+
+export const Store=configureStore({
+    reducer:{
+        
+    }
+}) 
+
+
