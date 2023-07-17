c10f649ce97dbc41e22ef282fd5d483407e04428
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-17 11:37:09 +0530

    created userslice

--- car land frontend/src/redux/Slices/UserSlice/UserSlice.tsx
+++ car land frontend/src/redux/Slices/UserSlice/UserSlice.tsx
@@ -1,9 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type User={
+    firstName :string ,
+    id: string ,
+    email: string,
+    image: string,
+    token: string
+}
+const initialState:User={
+firstName:'',
+id: '' ,
+email: '',
+image: '',
+token:''
+}
 
 
 const UserSlice= createSlice({
     name:"Users",
-    initialState:{},
-    reducers:{}
+    initialState,
+    reducers:{
+        setUserDetails : (state , action) =>{
+            state.firstName = action.payload.name;
+            state.id = action.payload.id
+            state.email = action.payload.email
+            state.image = action.payload?.image
+            state.token = action.payload.token
+          },
+    }
 })
+
+
+export const {setUserDetails}=UserSlice.actions
+
+export default UserSlice.reducer
