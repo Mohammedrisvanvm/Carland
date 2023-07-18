ea4f4827f58d49b478646072a12939165dc47b23
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-18 15:27:00 +0530

    model user Schema created

--- car land backend/src/models/userSchema.tsx
+++ car land backend/src/models/userSchema.tsx
@@ -1,3 +1,27 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, model,Document } from "mongoose";
+
+interface Iuser extends Document {
+    userName: String,
+    email: String,
+    password: String,
+
+}
+
+const userSchema: Schema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
 
 
+const userModel = model<Iuser>('user',userSchema)
