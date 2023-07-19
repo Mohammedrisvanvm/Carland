9a3844db0ea7c2112a5631155656de71c0fa9dce
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-19 14:13:55 +0530

    sign and verify jwt created and implimenting to code

--- car land backend/src/utils/jwtUtils/jwtutils.ts
+++ car land backend/src/utils/jwtUtils/jwtutils.ts
@@ -4,6 +4,17 @@ import jwt from 'jsonwebtoken'
 const privateKey=`qwerty`
 const publicKey=`ytrewq`
 
-export const jwtSign=(payload:object,expiresIn:string | number){
-    
+export const jwtSign=(payload:object,expiresIn:string | number)=>{
+  return  jwt.sign(payload,privateKey,{algorithm:'RS256',expiresIn})
+}
+
+export const verifyJwt=(token:string)=>{
+    try {
+        
+        const decoded=jwt.verify(token,publicKey)
+        return {payload:decoded,expiresIn:false}
+    } catch (error:any) {
+        return {payload:null,expiresIn:error.message.include('jwt expired')}
+        
+    }
 }
