5d2ca55121fdd9f6f9f2019540feb86ba7aa761e
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-19 15:24:47 +0530

    checking user token in cookie as a middleware

--- car land backend/src/controllers/userController/userAuth/authController.ts
+++ car land backend/src/controllers/userController/userAuth/authController.ts
@@ -1,50 +1,53 @@
-import {  Request, Response } from "express";
+import { Request, Response } from "express";
 import userModel from "../../../models/userSchema";
 import { IUser } from "../../../interfaces/userInterface";
 import AsyncHandler from "express-async-handler";
-import { jwtSign } from "../../../utils/jwtUtils/jwtutils";
+import { jwtSign, verifyJwt } from "../../../utils/jwtUtils/jwtutils";
 
-
-
-export const userSignUpController = AsyncHandler(async(req: Request, res: Response): Promise<void> => {
-
-    const { userName, email, password } = req.body.value
+export const userSignUpController = AsyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const { userName, email, password } = req.body.value;
     // const { userName, email, password } = req.body
 
     console.log(userName, email, password);
 
-    const userExist: any = await userModel.findOne({ email })
+    const userExist: any = await userModel.findOne({ email });
 
     if (userExist) {
-      throw new Error ('User Already Exists')
+      throw new Error("User Already Exists");
     } else {
-        const user: {} = await userModel.create({
-            userName,
-            email,
-            password
-        })
- 
-
-
-        res.status(201).json({ user })
+      const user: {} = await userModel.create({
+        userName,
+        email,
+        password,
+      });
+      res.status(201).json({ user });
     }
-})
-export const userLoginController = AsyncHandler(async (req: Request, res: Response): Promise<void> => {
-    const { email, password } = req.body
+  }
+);
+export const userLoginController = AsyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const { email, password } = req.body;
     // const { email, password } = req.body.value;
-    console.log(email,password);
-    
+    console.log(email, password);
+
     const userExist: IUser | null = await userModel.findOne({ email });
 
-
     if (userExist && (await userExist.matchPassword(password))) {
-console.log("hai");
+      console.log("hai");
 
-        const accesstoken= await jwtSign({id:userExist._id,name:userExist.userName,email:userExist.email},'1hr')
-        console.log(accesstoken);
-        res.status(200).json({ user:userExist });
+      const accesstoken = await jwtSign(
+        { id: userExist._id, name: userExist.userName, email: userExist.email },
+        "1hr"
+      );
+      console.log(accesstoken);
+      res
+        .status(200)
+        .cookie("accessToken", accesstoken, { maxAge: 300000, httpOnly: true })
+        .json({ user: userExist });
+         res.send(verifyJwt(accesstoken).payload)
     } else {
-      throw new Error ('Invalid email or password')
+      throw new Error("Invalid email or password");
     }
-})
-
+  }
+);
