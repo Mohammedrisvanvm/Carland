4f11717f1c1d3518fe6d39234f3a574d5410f855
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-19 16:15:07 +0530

    created refresh token

--- car land backend/src/controllers/userController/userAuth/authController.ts
+++ car land backend/src/controllers/userController/userAuth/authController.ts
@@ -3,51 +3,58 @@ import userModel from "../../../models/userSchema";
 import { IUser } from "../../../interfaces/userInterface";
 import AsyncHandler from "express-async-handler";
 import { jwtSign, verifyJwt } from "../../../utils/jwtUtils/jwtutils";
+import { createSession } from "../../../helpers/sessionController/sessionController";
 
 export const userSignUpController = AsyncHandler(
-  async (req: Request, res: Response): Promise<void> => {
-    const { userName, email, password } = req.body.value;
-    // const { userName, email, password } = req.body
+    async (req: Request, res: Response): Promise<void> => {
+        const { userName, email, password } = req.body.value;
+        // const { userName, email, password } = req.body
 
-    console.log(userName, email, password);
+        console.log(userName, email, password);
 
-    const userExist: any = await userModel.findOne({ email });
+        const userExist: any = await userModel.findOne({ email });
 
-    if (userExist) {
-      throw new Error("User Already Exists");
-    } else {
-      const user: {} = await userModel.create({
-        userName,
-        email,
-        password,
-      });
-      res.status(201).json({ user });
+        if (userExist) {
+            throw new Error("User Already Exists");
+        } else {
+            const user: {} = await userModel.create({
+                userName,
+                email,
+                password,
+            });
+            res.status(201).json({ user });
+        }
     }
-  }
 );
 export const userLoginController = AsyncHandler(
-  async (req: Request, res: Response): Promise<void> => {
-    const { email, password } = req.body;
-    // const { email, password } = req.body.value;
-    console.log(email, password);
+    async (req: Request, res: Response): Promise<void> => {
+        const { email, password } = req.body;
+        // const { email, password } = req.body.value;
+        console.log(email, password);
 
-    const userExist: IUser | null = await userModel.findOne({ email });
+        const userExist: IUser | null = await userModel.findOne({ email });
 
-    if (userExist && (await userExist.matchPassword(password))) {
-      console.log("hai");
+        if (userExist && (await userExist.matchPassword(password))) {
+            console.log("hai");
 
-      const accesstoken = await jwtSign(
-        { id: userExist._id, name: userExist.userName, email: userExist.email },
-        "1hr"
-      );
-      console.log(accesstoken);
-      res
-        .status(200)
-        .cookie("accessToken", accesstoken, { maxAge: 300000, httpOnly: true })
-        .json({ user: userExist });
-         res.send(verifyJwt(accesstoken).payload)
-    } else {
-      throw new Error("Invalid email or password");
+            const session=createSession(email,userExist.userName)
+            const accessToken = await jwtSign(
+                { id: userExist._id, name: userExist.userName, email: userExist.email,sessionId:session.sessionId },
+                "5s"
+            );
+            const refreshToken = await jwtSign(
+                { sessionId:session.sessionId },
+                "1y"
+            );
+      
+            res
+                .status(200)
+                .cookie("accessToken", accessToken, { maxAge: 300000, httpOnly: true })
+                .cookie("refreshtoken", refreshToken, { maxAge: 3.154e10, httpOnly: true })
+                .json({ user: userExist });
+            res.send(session)
+        } else {
+            throw new Error("Invalid email or password");
+        }
     }
-  }
 );
--- car land backend/src/helpers/sessionController/sessionController.ts
+++ car land backend/src/helpers/sessionController/sessionController.ts
@@ -1,5 +1,20 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 
-export function createSessionHandler(req:Request,res:Response){
+
+export const sessions: Record<
+    string,
+    { sessionId: string; email: string; valid: boolean }
+> = {};
+
+export function createSession(
+    email: string, name: String) {
+
+    const sessionId = String(Object.keys(sessions).length + 1);
+
+    const session = { sessionId, email, valid: true, name };
+
+    sessions[sessionId] = session;
+
+    return session;
 
 }
