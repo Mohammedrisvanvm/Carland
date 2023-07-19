97441cc4e036fbde83dc5e393a09f4255df36a04
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-19 10:56:40 +0530

    not found error handler created

--- car land backend/src/middlewares/errorHandler/errorHandlingMiddleware.ts
+++ car land backend/src/middlewares/errorHandler/errorHandlingMiddleware.ts
@@ -1,1 +1,6 @@
-const notFound
+import { Request,Response,NextFunction } from "express"
+const notFound=(req:Request,res:Response,next:NextFunction):void=>{
+const error=new Error (`Not Found -${req.originalUrl}`)
+res.status(404)
+next(error)
+}
