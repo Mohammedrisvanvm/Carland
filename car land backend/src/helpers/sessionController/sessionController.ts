6fe1411e189f31fb6d1550946167cbe3a96f7366
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-19 12:55:20 +0530

    creating jwt token

---
+++ car land backend/src/helpers/sessionController/sessionController.ts
@@ -0,0 +1,5 @@
+import { Request,Response } from "express";
+
+export function createSessionHandler(req:Request,res:Response){
+
+}
---
+++ car land backend/src/utils/jwtUtils/jwtutils.ts
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken'
+
+
+const privateKey=`qwerty`
+const publicKey=`ytrewq`
+
+export const jwtSign=(payload:object,expiresIn:string | number){
+    
+}
