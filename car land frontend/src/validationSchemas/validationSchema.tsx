e9625ce8a837124cda90e635fcf97be8b1023ba7
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-27 12:37:48 +0530

    validation schema creating for add car

--- car land frontend/src/validationSchemas/validationSchema.tsx
+++ car land frontend/src/validationSchemas/validationSchema.tsx
@@ -13,7 +13,10 @@ export const LoginSchema=yup.object().shape({
     email: yup.string().email('enter a valid email eg:risvan@gmail.com').required(),
     password: yup.string().trim().min(6).matches(password, { message: 'please enter password Atleast 6 characters eg:Huat5@' }).required(),
 })
-export const AddCarSchema=""
+export const AddCarSchema=yup.object().shape({
+    vehicleName: yup.string().trim().min(3).matches(/^[a-zA-Z0-9\s]+$/, 'Name must not contain special characters').required(),
+
+})
 
 
 
