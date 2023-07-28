3562d1632450ede8a4173f825d9945601aec84e4
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-07-28 20:30:43 +0530

    git error

--- car land frontend/src/services/apis/userApi/userApi.tsx
+++ car land frontend/src/services/apis/userApi/userApi.tsx
@@ -19,3 +19,6 @@ return axiosBase.get("/users/auth/userCheck")
 export const userLogout=():Promise<AxiosResponse>=>{
 return axiosBase.get("/users/auth/userLogout")
 }
+export const kahs=():Promise<AxiosResponse>=>{
+return axiosBase.get("/users/auth/userLogout")
+}
