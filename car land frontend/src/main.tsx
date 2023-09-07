deb6cc6d202e6ea71878aff69c74721271b8629d
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-09-07 12:14:45 +0530

    provided persist in main.tsx

--- car land frontend/src/main.tsx
+++ car land frontend/src/main.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { Store } from './redux/store/store.tsx'
-import {GoogleOAuthProvider} from '@react-oauth/google'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
 
-const GOOGLE_CLIENT_ID='198622255417-c11hpmsp56010plhdb80gda35sacanlb.apps.googleusercontent.com'
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Store, persistor } from "./redux/store/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const GOOGLE_CLIENT_ID =
+  "198622255417-c11hpmsp56010plhdb80gda35sacanlb.apps.googleusercontent.com";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={Store}>
-      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID} >
-    <App />
-      </GoogleOAuthProvider>
-    </Provider >
-  </React.StrictMode>,
-)
+      <PersistGate persistor={persistor}>
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+          <App />
+        </GoogleOAuthProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
