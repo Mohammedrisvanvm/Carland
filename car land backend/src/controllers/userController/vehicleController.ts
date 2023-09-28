e68d28a994c57fa40dbab558f129aee17a34b851
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-09-28 10:44:27 +0530

    pagination implimented in backend

--- car land backend/src/controllers/userController/vehicleController.ts
+++ car land backend/src/controllers/userController/vehicleController.ts
@@ -3,7 +3,17 @@ import AsyncHandler from "express-async-handler";
 import IVehicle from "../../interfaces/vehicleInterface";
 import vehicleModel from "../../models/vehicleSchema";
 export const userVehicles = AsyncHandler(
-    async (req: Request, res: Response): Promise<any> => {
-const vehicles:IVehicle[]=await vehicleModel.find()
-res.json({vehicles})
-    })
+  async (req: Request, res: Response): Promise<any> => {
+   
+    const pageNumber: number = Number(req.query.currentPage);
+
+    const vehicles: IVehicle[] = await vehicleModel
+      .find()
+      .skip((pageNumber - 1) * 4)
+      .limit(4);
+    const count: number = await vehicleModel.find().count();
+    console.log(vehicles, "vehicles");
+
+    res.json({ vehicles, count });
+  }
+);
