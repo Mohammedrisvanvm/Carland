63a0a970d9d1b75360bcb8210a1412c32b9bcf81
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-10-02 20:19:28 +0530

    frontend booking details Interface created

---
+++ car land frontend/src/interfaces/booklingConfirmInterface.ts
@@ -0,0 +1,23 @@
+interface IBook extends Document {
+    _id: string;
+    hubId: string;
+    vehicleId: string;
+    userId: string;
+    hubName:string,
+    vehicleName:string,
+    bookingStartDate: Date;
+    totalPrice: number;
+    carPrice:number
+    days: number;
+    bookingEndDate: Date;
+    pickuptime: string;
+    paymentDetails:{
+      razorpay_payment_id: string;
+      razorpay_order_id: string;
+      razorpay_signature: string;
+    }
+    paymentStatus:string
+    status: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+  }
