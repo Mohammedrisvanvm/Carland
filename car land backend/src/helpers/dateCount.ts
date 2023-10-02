cb126c1bfc20b0ba772af054ffcf813cd2c0814d
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-10-02 17:08:08 +0530

    date count function created

--- car land backend/src/helpers/dateCount.ts
+++ car land backend/src/helpers/dateCount.ts
@@ -1,12 +1,9 @@
-export function dateCount(pickUpDate:string,dropDate:string):number {
-    var date1:Date = new Date(pickUpDate);
-        var date2:Date = new Date(dropDate);
-          
-      
-        var Difference_In_Time = date2.getTime() - date1.getTime();
-        console.log(typeof(Difference_In_Time));
-          
-        
-        var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-      return Difference_In_Days
-    }
+export function dateCount(pickUpDate: string, dropDate: string): number {
+  var date1: Date = new Date(pickUpDate);
+  var date2: Date = new Date(dropDate);
+
+  var Difference_In_Time: number = date2.getTime() - date1.getTime();
+
+  var Difference_In_Days: number = Difference_In_Time / (1000 * 3600 * 24);
+  return Difference_In_Days;
+}
