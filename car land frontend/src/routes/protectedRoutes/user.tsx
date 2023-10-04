46d194c070b8fe1d146998ef81113a4e7f870ed3
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-10-04 10:07:18 +0530

    created user protected route

---
+++ car land frontend/src/routes/protectedRoutes/user.tsx
@@ -0,0 +1,43 @@
+import React, { Children, useEffect, useState } from "react";
+import { Outlet, Navigate, useLocation, useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../redux/store/storeHook";
+
+interface Props {
+  children: React.ReactNode;
+  role: string;
+}
+
+const ProtectedRouteuser = ({ children, role }: Props): React.ReactNode => {
+  const user = useAppSelector((state) => state.user);
+
+  const location = useLocation();
+
+  if (role === "user" && user.accessToken) {
+    console.log(location.pathname);
+
+    if (
+      location.pathname === "/userlogin" ||
+      location.pathname === "/usersignup" ||
+      location.pathname === "/userotp" ||
+      location.pathname === "/userauth"
+    ) {
+      return <Navigate to={"/"} />;
+    } else {
+      return children;
+    }
+  } else {
+    console.log(location.pathname);
+    if (
+      location.pathname === "/userlogin" ||
+      location.pathname === "/usersignup" ||
+      location.pathname === "/userotp" ||
+      location.pathname === "/userauth"
+    ) {
+      return children;
+    } else {
+      return <Navigate to={"/userauth"} />;
+    }
+  }
+};
+
+export default ProtectedRouteuser;
