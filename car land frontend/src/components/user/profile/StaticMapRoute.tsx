80de5a975e1ccf3c480989e4da631285c15fb2e8
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-10-11 14:07:12 +0530

    default marker added

--- car land frontend/src/components/user/profile/StaticMapRoute.tsx
+++ car land frontend/src/components/user/profile/StaticMapRoute.tsx
@@ -11,12 +11,15 @@ const StaticMapRoute = () => {
             container:"map",
             style:'mapbox://styles/mapbox/streets-v11',
             zoom:8,
-        
             center: [76.2711, 10.8505],
+           
         })
-      
-        return ()=>map.remove();
+        
+      new mapboxgl.Marker().setLngLat([76.2711, 10.8505]).addTo(map)
+     return ()=>map.remove();
     },[])
+   
+     
   return (
     <div>
        <div className="flex flex-col items-center">
