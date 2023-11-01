ed5674340ccd15d0a70ee4d31e4ecc1cc89d4b48
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-11-01 11:12:46 +0530

    :sparkles:button and base 64 converter added

--- car land frontend/src/utils/toastConfig.ts
+++ car land frontend/src/utils/toastConfig.ts
@@ -1,6 +1,11 @@
 import { toast } from "react-toastify";
 
 export const toastHelper = (service: string, message: string) => {
-  if (service === "success") toast.success(message);
-  if (service === "error") toast.error(message);
+  console.log(message);
+
+  if (service === "success") {
+    toast.success(message);
+  } else {
+    toast.error(message);
+  }
 };
