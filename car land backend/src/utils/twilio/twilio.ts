a1efd8d4224fb88e1430fb4cacae1e9630439c86
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-11-04 16:03:38 +0530

    bug fixing

--- car land backend/src/utils/twilio/twilio.ts
+++ car land backend/src/utils/twilio/twilio.ts
@@ -1,31 +1,25 @@
 import Twilio from "twilio";
-const accountid: string | undefined = process.env.TWILIO_ACCOUNT_SID;
-const Auth: string | undefined = process.env.AUTH_TOKEN;
 
-const client = Twilio(accountid, Auth);
+const client = Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.AUTH_TOKEN);
 
 export function getotp() {
-    const min = 100000; 
-    const max = 999999;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
+  const min = 100000;
+  const max = 999999;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  
-
-
-export const sendOtp = async (phoneNumber: number):Promise<number> => {
+export const sendOtp = async (phoneNumber: number): Promise<number> => {
   try {
-    const otp=getotp()
-  //  await client.messages
-  //     .create({
-  //       body: `otp verification from carland  ${otp}`,
-  //       to: `+91${phoneNumber}`, 
-  //       from: "++1 267 362 9139", 
-  //     })
-  //     .then((message) => console.log(message.sid))
-  //     .catch((error) => console.error("Error sending message:", error));
-    return otp
+    const otp = getotp();
+    await client.messages
+      .create({
+        body: `otp verification from carland  ${otp}`,
+        to: `+91${phoneNumber}`,
+        from: "++1 267 362 9139",
+      })
+      .then((message) => console.log(message.sid))
+      .catch((error) => console.error("Error sending message:", error));
+    return otp;
   } catch (error: any) {
     throw new Error(error);
   }
