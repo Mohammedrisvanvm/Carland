7f6166f8a874f73101552774d27d709e1389398f
Mohammed risvan vm <119659849+Mohammedrisvanvm@users.noreply.github.com>
2024-03-22 15:25:44 +0530

    Delete car land backend/build directory

--- car land backend/build/src/routers/adminRoutes/adminSalesReport.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const adminSalesReportManagement_1 = require("../../controllers/adminController/adminSalesReportManagement");
-const adminSalesReport = (0, express_1.Router)();
-adminSalesReport.get("/fetchreport", adminSalesReportManagement_1.salesReportAdmin);
-exports.default = adminSalesReport;
-//# sourceMappingURL=adminSalesReport.js.map
