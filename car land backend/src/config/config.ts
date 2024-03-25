aac13e3ecd0df1d451c1a85fd4a55661c1cd5ebc
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2024-03-25 16:28:44 +0530

    fixing uri error

--- car land backend/src/config/config.ts
+++ car land backend/src/config/config.ts
@@ -8,6 +8,7 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || ''
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ''
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@carland.zzuutla.mongodb.net/Carland`
 
+console.log(MONGO_URL);
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3131
 
