28849871b3d6ef8f4448d4dbee29304a64b7ee3b
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2024-03-26 12:39:31 +0530

    default port changed

--- car land backend/src/config/config.ts
+++ car land backend/src/config/config.ts
@@ -1,24 +1,20 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 // dotenv.config({path:"../../.env"})
 
-
-const MONGO_USERNAME = process.env.MONGO_USERNAME || ''
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ''
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@carland.zzuutla.mongodb.net/Carland`
-
-console.log(MONGO_URL);
-
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3131
-
-
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@carland.zzuutla.mongodb.net/Carland`;
+const SERVER_PORT = process.env.SERVER_PORT
+  ? Number(process.env.SERVER_PORT)
+  : 3000;
 
 export const config = {
-    mongo: {
-        url: MONGO_URL
-    },
-    server: {
-        port: SERVER_PORT 
-    }
-}
+  mongo: {
+    url: MONGO_URL,
+  },
+  server: {
+    port: SERVER_PORT,
+  },
+};
