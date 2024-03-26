626bf98ba9eb83021e69c8d116aa1d720e0606ee
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2024-03-26 12:48:41 +0530

    env bug

--- car land backend/build/src/config/config.js
+++ car land backend/build/src/config/config.js
@@ -5,11 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-// dotenv.config({path:"../../.env"})
+// dotenv.config();
+dotenv_1.default.config({ path: "../../.env" });
 const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@carland.zzuutla.mongodb.net/Carland`;
+console.log(MONGO_URL);
 const SERVER_PORT = process.env.SERVER_PORT
     ? Number(process.env.SERVER_PORT)
     : 3000;
