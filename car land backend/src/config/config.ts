73bddae237ce64093f6c9c1153cb763d7de4a14a
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2024-03-30 09:32:47 +0530

    env path updated

--- car land backend/src/config/config.ts
+++ car land backend/src/config/config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 
-dotenv.config();
-// dotenv.config({path:"../../.env"})
+// dotenv.config();
+dotenv.config({path:"../../.env"})
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
